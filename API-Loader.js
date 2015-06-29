231183a170fb8ad322ee70926577cd7e54cdf580
Samuel <ingenierofiestero@gmail.com>
2015-06-29 21:11:25 +0200

    mejoradi

--- API-Loader.js
+++ API-Loader.js
@@ -1,3 +1,46 @@
+/*
+	ComPis: Aplicacion para trabajar con la API del ayuntamiento de zaragoza
+	
+	Utiliza algoritmos para encontrar datos dentro de la descripcion y el titulo como el precio, la calle, la zona...
+*/
+
+var anuncio_juventud = {};
+anuncio_juventud.URL= "http://www.zaragoza.es/api/recurso/cultura-ocio/anuncio-juventud";
+anuncio_juventud.peticion ="?start=0&rows=2000";
+anuncio_juventud.insertar_anuncio="http://www.zaragoza.es/ciudad/sectores/jovenes/cipaj/cont/insertaranuncios.htm"; 
+	//URL de la API de anuncios para jovenes
+	/*
+	fl : Listado de atributos separados por comas que
+		se desea obtener en la respuesta.
+	rf :  Formato de textos enriquecidos, por defecto html.
+	start : Posición del primer registro que se obtendra.
+	rows : Numero de filas que se obtendran.
+	sort : Ordenacion de los campos.
+	q : Consulta con sintaxis FIQL.
+	?fl=...&rf=...&start=...&rows=...&sort=...&sort=...&q=...
+	*/
+	/*
+	{
+	  "contacto": "string",
+	  "title": "string",
+	  "provincia": "string",
+	  "description": "string",
+	  "creationDate": "Date",
+	  "tipo": "TipoAnuncio",
+	  "pais": "string",
+	  "poblacion": "string",
+	  "id": "int"
+	}
+	id = 1 "Alojamientos (demanda)";
+		2 "Alojamientos (oferta)";
+		9 "Gente"; 
+		10 "Intercambios - compartimos"; 
+	*/
+
+var estaciones_bici = {};
+estaciones_bici.URL = "http://www.zaragoza.es/api/recurso/urbanismo-infraestructuras/estacion-bicicleta";
+estaciones_bici.peticion="?start=0&rows=2000&srsname=wgs84";
+
 var descripcion_ejemplo = "Se alquila piso para estudiantes de septiembre a junio. Tres habitaciones, 2º con ascensor, amueblado, con calefacción y aire acondicionado";
 angular
   .module('ComPiApp', [])
@@ -16,7 +59,7 @@ function controlador($scope, $http){
 		    	filtro_ID(data.result,function(vector){
 		    		vm.anuncio_lista = vector;
 		    		vector.forEach(function(dato,i){
-		    			var cadena = remover_acentos(dato.title + "." + dato.description);
+		    			var cadena = remover_acentos(dato.title + ". " + dato.description);
 		    			var resultado = filtrar(dividir_cadena(cadena));
 		    			resultado.titulo = dato.title;
 		    			resultado.descripcion = dato.description;
@@ -31,6 +74,12 @@ function controlador($scope, $http){
 			}).error(function(data, status, headers, config) {     
 				alert("Ha fallado la petición. Estado HTTP:"+status);
 		});
+	}
+	vm.anuncios_filtrados;
+	vm.filtro = {};//Filtro de busquedas
+	vm.filtro.genero = "Ambos";
+	vm.filtrar = function(){
+
 	}
 }
 function filtro_ID(datos,cb){
@@ -103,11 +152,11 @@ function filtrar(frases){
 		}
 	});
 	//Zona
-	claves.push(["zona","proximo","cerca","sector","estacion","hospital","hotel"]);
+	claves.push(["zona","proximo","cerca","sector","estacion","hospital","hotel","universidad","cps","facultad"]);
 	cb.push(function(i,j,w){
 		if(encontrados[j] === false){
 			var z = i + 1;
-			respuesta.zona = buscar_mayusculas(frase.slice(i+1,i+3)).join(" ");
+			respuesta.zona = claves[j][w]+ " " +  buscar_mayusculas(frase.slice(i+1,i+3)).join(" ");
 			encontrados[j] = true;
 		}
 	});
@@ -133,7 +182,7 @@ function filtrar(frases){
 	return respuesta;
 }
 function remover_acentos(str) {
-	var map={'À':'A','Á':'A','Â':'A','Ã':'A','Ä':'A','Å':'A','Æ':'AE','Ç':'C','È':'E','É':'E','Ê':'E','Ë':'E','Ì':'I','Í':'I','Î':'I','Ï':'I','Ð':'D','Ò':'O','Ó':'O','Ô':'O','Õ':'O','Ö':'O','Ø':'O','Ù':'U','Ú':'U','Û':'U','Ü':'U','Ý':'Y','ß':'s','à':'a','á':'a','â':'a','ã':'a','ä':'a','å':'a','æ':'ae','ç':'c','è':'e','é':'e','ê':'e','ë':'e','ì':'i','í':'i','î':'i','ï':'i','ñ':'n','ò':'o','ó':'o','ô':'o','õ':'o','ö':'o','ø':'o','ù':'u','ú':'u','û':'u','ü':'u','ý':'y','ÿ':'y','Ā':'A','ā':'a','Ă':'A','ă':'a','Ą':'A','ą':'a','Ć':'C','ć':'c','Ĉ':'C','ĉ':'c','Ċ':'C','ċ':'c','Č':'C','č':'c','Ď':'D','ď':'d','Đ':'D','đ':'d','Ē':'E','ē':'e','Ĕ':'E','ĕ':'e','Ė':'E','ė':'e','Ę':'E','ę':'e','Ě':'E','ě':'e','Ĝ':'G','ĝ':'g','Ğ':'G','ğ':'g','Ġ':'G','ġ':'g','Ģ':'G','ģ':'g','Ĥ':'H','ĥ':'h','Ħ':'H','ħ':'h','Ĩ':'I','ĩ':'i','Ī':'I','ī':'i','Ĭ':'I','ĭ':'i','Į':'I','į':'i','İ':'I','ı':'i','Ĳ':'IJ','ĳ':'ij','Ĵ':'J','ĵ':'j','Ķ':'K','ķ':'k','Ĺ':'L','ĺ':'l','Ļ':'L','ļ':'l','Ľ':'L','ľ':'l','Ŀ':'L','ŀ':'l','Ł':'L','ł':'l','Ń':'N','ń':'n','Ņ':'N','ņ':'n','Ň':'N','ň':'n','ŉ':'n','Ō':'O','ō':'o','Ŏ':'O','ŏ':'o','Ő':'O','ő':'o','Œ':'OE','œ':'oe','Ŕ':'R','ŕ':'r','Ŗ':'R','ŗ':'r','Ř':'R','ř':'r','Ś':'S','ś':'s','Ŝ':'S','ŝ':'s','Ş':'S','ş':'s','Š':'S','š':'s','Ţ':'T','ţ':'t','Ť':'T','ť':'t','Ŧ':'T','ŧ':'t','Ũ':'U','ũ':'u','Ū':'U','ū':'u','Ŭ':'U','ŭ':'u','Ů':'U','ů':'u','Ű':'U','ű':'u','Ų':'U','ų':'u','Ŵ':'W','ŵ':'w','Ŷ':'Y','ŷ':'y','Ÿ':'Y','Ź':'Z','ź':'z','Ż':'Z','ż':'z','Ž':'Z','ž':'z','ſ':'s','ƒ':'f','Ơ':'O','ơ':'o','Ư':'U','ư':'u','Ǎ':'A','ǎ':'a','Ǐ':'I','ǐ':'i','Ǒ':'O','ǒ':'o','Ǔ':'U','ǔ':'u','Ǖ':'U','ǖ':'u','Ǘ':'U','ǘ':'u','Ǚ':'U','ǚ':'u','Ǜ':'U','ǜ':'u','Ǻ':'A','ǻ':'a','Ǽ':'AE','ǽ':'ae','Ǿ':'O','ǿ':'o'};
+	var map={'À':'A','Á':'A','Â':'A','Ã':'A','Ä':'A','Å':'A','Æ':'AE','Ç':'C','È':'E','É':'E','Ê':'E','Ë':'E','Ì':'I','Í':'I','Î':'I','Ï':'I','Ð':'D','Ò':'O','Ó':'O','Ô':'O','Õ':'O','Ö':'O','Ø':'O','Ù':'U','Ú':'U','Û':'U','Ü':'U','Ý':'Y','ß':'s','à':'a','á':'a','â':'a','ã':'a','ä':'a','å':'a','æ':'ae','ç':'c','è':'e','é':'e','ê':'e','ë':'e','ì':'i','í':'i','î':'i','ï':'i','ò':'o','ó':'o','ô':'o','õ':'o','ö':'o','ø':'o','ù':'u','ú':'u','û':'u','ü':'u','ý':'y','ÿ':'y','Ā':'A','ā':'a','Ă':'A','ă':'a','Ą':'A','ą':'a','Ć':'C','ć':'c','Ĉ':'C','ĉ':'c','Ċ':'C','ċ':'c','Č':'C','č':'c','Ď':'D','ď':'d','Đ':'D','đ':'d','Ē':'E','ē':'e','Ĕ':'E','ĕ':'e','Ė':'E','ė':'e','Ę':'E','ę':'e','Ě':'E','ě':'e','Ĝ':'G','ĝ':'g','Ğ':'G','ğ':'g','Ġ':'G','ġ':'g','Ģ':'G','ģ':'g','Ĥ':'H','ĥ':'h','Ħ':'H','ħ':'h','Ĩ':'I','ĩ':'i','Ī':'I','ī':'i','Ĭ':'I','ĭ':'i','Į':'I','į':'i','İ':'I','ı':'i','Ĳ':'IJ','ĳ':'ij','Ĵ':'J','ĵ':'j','Ķ':'K','ķ':'k','Ĺ':'L','ĺ':'l','Ļ':'L','ļ':'l','Ľ':'L','ľ':'l','Ŀ':'L','ŀ':'l','Ł':'L','ł':'l','Ń':'N','ń':'n','Ņ':'N','ņ':'n','Ň':'N','ň':'n','ŉ':'n','Ō':'O','ō':'o','Ŏ':'O','ŏ':'o','Ő':'O','ő':'o','Œ':'OE','œ':'oe','Ŕ':'R','ŕ':'r','Ŗ':'R','ŗ':'r','Ř':'R','ř':'r','Ś':'S','ś':'s','Ŝ':'S','ŝ':'s','Ş':'S','ş':'s','Š':'S','š':'s','Ţ':'T','ţ':'t','Ť':'T','ť':'t','Ŧ':'T','ŧ':'t','Ũ':'U','ũ':'u','Ū':'U','ū':'u','Ŭ':'U','ŭ':'u','Ů':'U','ů':'u','Ű':'U','ű':'u','Ų':'U','ų':'u','Ŵ':'W','ŵ':'w','Ŷ':'Y','ŷ':'y','Ÿ':'Y','Ź':'Z','ź':'z','Ż':'Z','ż':'z','Ž':'Z','ž':'z','ſ':'s','ƒ':'f','Ơ':'O','ơ':'o','Ư':'U','ư':'u','Ǎ':'A','ǎ':'a','Ǐ':'I','ǐ':'i','Ǒ':'O','ǒ':'o','Ǔ':'U','ǔ':'u','Ǖ':'U','ǖ':'u','Ǘ':'U','ǘ':'u','Ǚ':'U','ǚ':'u','Ǜ':'U','ǜ':'u','Ǻ':'A','ǻ':'a','Ǽ':'AE','ǽ':'ae','Ǿ':'O','ǿ':'o'};
 	var res="";
 	for (var i=0;i<str.length;i++){
 		c=str.charAt(i);
@@ -245,47 +294,11 @@ function prueba(){
 	var resultado = filtrar(dividir_cadena(cadena));
 	console.log(resultado);
 }
-
-var anuncio_juventud = {};
-anuncio_juventud.URL= "http://www.zaragoza.es/api/recurso/cultura-ocio/anuncio-juventud";
-anuncio_juventud.peticion ="?start=0&rows=2000";
-anuncio_juventud.insertar_anuncio="http://www.zaragoza.es/ciudad/sectores/jovenes/cipaj/cont/insertaranuncios.htm"; 
-	//URL de la API de anuncios para jovenes
-	/*
-	fl : Listado de atributos separados por comas que
-		se desea obtener en la respuesta.
-	rf :  Formato de textos enriquecidos, por defecto html.
-	start : Posición del primer registro que se obtendra.
-	rows : Numero de filas que se obtendran.
-	sort : Ordenacion de los campos.
-	q : Consulta con sintaxis FIQL.
-	?fl=...&rf=...&start=...&rows=...&sort=...&sort=...&q=...
-	*/
-	/*
-	{
-	  "contacto": "string",
-	  "title": "string",
-	  "provincia": "string",
-	  "description": "string",
-	  "creationDate": "Date",
-	  "tipo": "TipoAnuncio",
-	  "pais": "string",
-	  "poblacion": "string",
-	  "id": "int"
-	}
-	id = 1 "Alojamientos (demanda)";
-		2 "Alojamientos (oferta)";
-		9 "Gente"; 
-		10 "Intercambios - compartimos"; 
-	*/
-
-var estaciones_bici = {};
-estaciones_bici.URL = "http://www.zaragoza.es/api/recurso/urbanismo-infraestructuras/estacion-bicicleta";
-estaciones_bici.peticion="?start=0&rows=2000&srsname=wgs84";
-
-/* Analizador sintáctico de oraciones. 
-Elimina la paja (palabras menores de 2 letras que no sean numeros) 
-asi como preposiciones, articulos, conjunciones...
+/* 
+	Analizador sintáctico de oraciones:
+	
+	Elimina la paja (palabras menores de 2 letras que no sean numeros) 
+	asi como preposiciones, articulos, conjunciones...
 */
 var clave = ["los","las","ante", "bajo", "con", "contra","desde","entre", "durante", "hacia", "hasta", "mediante", "para", "por", "según", "sin", "sobre", "tras","pero","sino","que","como","mas","porque","puesto","luego","con","para","aunque","muy"];
 function eliminar_paja(cadena){
